c462301a62effef2bf717730210fa9d031a77d2a
soo-ing <sooing5412@naver.com>
2021-10-22 21:20:53 +0900

    commit

---
+++ PHP/dht11.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <form action="process.php" method="get">
+          <p>Temperature : <input type="text" name="temp"></p>
+          <p>Humidity : <input type="text" name="humi"></p>
+          <p>Weather : <input type="text" name="weather"></p>
+          <p><input type="submit" /></p>
+        </form>
+    </body>
+</html>
