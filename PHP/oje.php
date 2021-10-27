1c928feaeee8369637390217f15d38cf2ee68e29
soo-ing <sooing5412@naver.com>
2021-10-27 21:20:58 +0900

    commit

---
+++ PHP/oje.php
@@ -0,0 +1,10 @@
+<html>
+    <head>test</head>
+    <body>
+      
+        <script>
+            window.location.href = "http://192.168.137.169:8000/index.html";
+        </script>
+       
+    </body>
+</html>
